d995d667483b6d0f3b120594a206f241637df141
ThovenhairWorrior <wswm2007@naver.com>
2023-05-05 16:45:06 +0900

    5-5: 이미지 변경 추가

--- active.js
+++ active.js
@@ -3,10 +3,20 @@ const btnFool = document.querySelector(".answer");
 const btnPicture = document.querySelector(".btn-close");
 const answer = document.querySelector("input");
 const dim = document.querySelector(".dim");
+const picture = document.querySelector(".luppy");
+const dead = document.querySelector(".dead");
+const afterBtn = document.querySelector(".afterBtn");
+
+
+
+
 
 
 function openPopup(){
     article.classList.add("active");
+    picture.classList.add("active");
+    dead.classList.add("active");
+    afterBtn.classList.add("active");
 }
 
 function closePopup(){
